382b88f246503d114b5b29bc46302d8b92780cba
Marek Michalak <michalak.marek@gmail.com>
2023-01-27 00:09:50 +0100

    add animated gif

--- LostFocus/ViewModels/FirstChildViewModel.cs
+++ LostFocus/ViewModels/FirstChildViewModel.cs
@@ -9,14 +9,37 @@ namespace LostFocus.ViewModels
 {
     public class FirstChildViewModel : ViewModelBase, IFirstChildViewModel
     {
-        public ActionRelayMethod PumpControlCommand { get; set; }
-
+        #region Fields
         private readonly NavigationStore _navigationStore;
+        #endregion
+        #region Property
+        public ActionRelayMethod PumpControlCommand { get; set; }
+        public ActionRelayMethod StartAnimatedGifCommand { get; set; }
+
+        private string _selectedImage;
+
+        public string SelectedImage
+        {
+            get { return _selectedImage; }
+            set { _selectedImage = value;
+                OnPropertyChanged("SelectedImage");
+            }
+        }
+
+        #endregion
+
         public FirstChildViewModel(NavigationStore navigationStore)
         {
             _navigationStore = navigationStore;
             PumpControlCommand = new ActionRelayMethod(p => EnterButton());
+            StartAnimatedGifCommand = new ActionRelayMethod(p => StartAnimatedGif());
+            
             //Parameterless constructor
+        }
+
+        private void StartAnimatedGif()
+        {
+            SelectedImage = @"../Images/working.gif";
         }
 
         public void EnterButton()
